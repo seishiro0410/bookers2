96617d14536bdf786c63f8d459498ef1f7b25f2f
Seishiro Fukuda <108728183+seishiro0410@users.noreply.github.com>
2022-08-22 15:07:17 +0000

    step1

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,25 @@
+class BookController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+  
+  def index
+    
+  end
+
+  def show
+  end
+  
+  private
+  
+  def book_params
+    params.repuire(:book).permit(:title, :body)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_21_154047) do
+ActiveRecord::Schema.define(version: 2022_08_22_134516) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
